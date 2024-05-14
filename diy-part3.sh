3f95f706ff7b431ee1e2ebb843d029f13785f00c
JAXHA <hai33590@163.com>
2024-05-14 10:37:06 +0800

    Update diy-part3.sh
    注释掉2sh 里面有的

--- diy-part3.sh
+++ diy-part3.sh
@@ -11,13 +11,13 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
 #删除默认登陆密码
-sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+# sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 
 # 预置openclash内核
-mkdir -p files/etc/openclash/core
+# mkdir -p files/etc/openclash/core
 
 
 # dev内核
