16c75d5798c8cc75051c4bf3bff609e17ba81290
JAXHA <hai33590@163.com>
2024-05-26 09:43:56 +0800

    Update diy-part1.sh
    增加open clash

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
 echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
-#echo "src-git OpenClash https://github.com/vernesong/OpenClash.git" >> ./feeds.conf.default
+echo "src-git OpenClash https://github.com/vernesong/OpenClash.git" >> ./feeds.conf.default
 
 #添加额外软件包
 #git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
